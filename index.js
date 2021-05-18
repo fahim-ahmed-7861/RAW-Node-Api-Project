830e72ca07659b8231e06a195e7942fb42831c59
fahim-ahmed-7861 <fahimahmedshojib109@gmai.com>
2021-05-18 21:56:02 +0600

    done

--- index.js
+++ index.js
@@ -7,77 +7,20 @@
  */
 
 // dependencies
+const server = require('./lib/server');
+const workers = require('./lib/worker');
 
-const http = require('http');
-const {handleReqRes} = require('./helpers/handleReqRes')
-const environment = require('./helpers/environments')
-const data = require('./lib/data')
-
-const {sendTwilioSms} =require('./helpers/notification');
-
-
-// app-object module scaffolding
+// app object - module scaffolding
 const app = {};
 
-//Testing : pore muche dibo
-
-sendTwilioSms('01778482201','Hello Word',(err)=>
-{
-    console.log('this is a error message ',err);
-})
-
-
-/*data.delete('test','newfile',err=>{
-    
-
-    console.log(err)
-
-})
-
-/*data.update('test','newfile',{name : 'india', language : 'hindi'},err=>{
-    
-
-    console.log(err)
-
-})*/
-
-/*data.read('test','newfile',(err,data)=>
-{
-    console.log(err,data);
-})*/
-
-/*data.create('test','newfile',{name : 'bangladesh', language : 'bangla'},(err)=>
-{
-    console.log('error was : ',err);
-})*/
-
-
-
-
-//configuration 
-app.config = {
-
-    port : 3000
+app.init = () => {
+    // start the server
+    server.init();
+    // start the workers
+    workers.init();
 };
 
-//createServer
-app.createServer = ()=>
-{
-    const server = http.createServer(app.handleReqRes);
+app.init();
 
-    server.listen(environment.port,()=>
-    {
-       
-        console.log(`listening to port ${environment.port}`)
-    });
-
-}
-
-
-// handle response request
-
-app.handleReqRes = handleReqRes;
-
-// start the server
-
-app.createServer();
+// export the app
+module.exports = app;
